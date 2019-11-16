50f6310b6813d5a8d14668bb93946203c37d0e40
drgwses <drgwses@qq.com>
2019-11-16 21:40:17 +0800

    wwwwwwwwwwwwwwwwww

---
+++ src/main/java/com/text/text/Pojo.java
@@ -0,0 +1,25 @@
+package com.text.text;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Pojo {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/text/text/controller.java
+++ src/main/java/com/text/text/controller.java
@@ -1,13 +1,23 @@
 package com.text.text;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class controller {
+    @Autowired
+    private Pojo pojo;
     @RequestMapping("/test2222")
-    public String text(){
-    //    System.out.println(123456789);
-        return null;
+    @ResponseBody
+    public Pojo text(){
+        System.out.println(123456789);
+        pojo.setUsername("123");
+        pojo.setPassword("123");
+        return pojo;
     }
 }
