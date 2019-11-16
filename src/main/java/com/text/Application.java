8d7573fc1586b67aaee1be4a90bb28bc736c6c14
drgwses <drgwses@qq.com>
2019-11-16 19:07:24 +0800

    瓦达带我打

---
+++ src/main/java/com/text/Application.java
@@ -0,0 +1,10 @@
+package com.text;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+         SpringBootApplication application=new SpringBootApplication(Application.class)
+    }
+}
